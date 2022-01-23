9900e6d921fd0d8a093c671785259cf2c05e05b4
Ade Rohayat <muhamadaderohayat122@gmail.com>
2022-01-23 07:50:08 +0700

    Menerapkan MongoDB kedalam ExpressJS (CRUD) - PART 2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const router = require('./routers')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
 app.use(router)
 
 app.listen(port, () => {
--- routers.js
+++ routers.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { ObjectId } = require('mongodb');
 const router = express.Router()
 const connection = require('./connection')
 
@@ -12,6 +13,73 @@ router.get('/users', async (req, res) => {
       const db = connection.db('bwa_storegg')
       const users = await db.collection('users').find().toArray()
       res.send({data: users})
+    }else{
+      res.send({message: 'gagal load database'})
+    }
+  } catch (err) {
+    res.send({message: err.message || 'server error'})
+  }
+})
+
+router.post('/users', async (req, res) => {
+  try {
+    if(connection.connect()){
+      const {name, email, status} = req.body
+      const db = connection.db('bwa_storegg')
+      const users = await db.collection('users').insertOne({
+        name, 
+        email, 
+        status
+      })
+      if(users.insertedId){
+        res.send({code: 200, data: users})
+      }else{
+        res.send({code:500, message: 'gagal menambahkan data'})
+      }
+    }else{
+      res.send({message: 'gagal load database'})
+    }
+  } catch (err) {
+    res.send({message: err.message || 'server error'})
+  }
+})
+
+router.put('/users/:id', async (req, res) => {
+  try {
+    if(connection.connect()){
+      const {id} = req.params
+      const {name, email, status} = req.body
+      const db = connection.db('bwa_storegg')
+      const users = await db.collection('users').updateOne({_id: ObjectId(id)}, {$set : {
+        name, 
+        email, 
+        status
+      }})
+      if(users.matchedCount){
+        res.send({code: 200, data: users})
+      }else{
+        res.send({code: 500, message: 'gagal mengupdate data'})
+      }
+    }else{
+      res.send({message: 'gagal load database'})
+    }
+  } catch (err) {
+    res.send({message: err.message || 'server error'})
+  }
+})
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    if(connection.connect()){
+      const {id} = req.params
+      const {name, email, status} = req.body
+      const db = connection.db('bwa_storegg')
+      const users = await db.collection('users').deleteOne({_id: ObjectId(id)})
+      if(users.deletedCount){
+        res.send({code: 200, data: users})
+      }else{
+        res.send({code: 500, message: 'gagal delete data'})
+      }
     }else{
       res.send({message: 'gagal load database'})
     }
